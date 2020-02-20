7a73b0ae1b8fd88860358e988fb6bb1b84fa36ed
acai66 <1779864536@qq.com>
2020-02-20 16:47:16 +0800

    debug live2d

--- sakura-app.js
+++ sakura-app.js
@@ -1556,7 +1556,7 @@ var home = location.href,
 		        n = $(".openNav");
 			o == e && (a.removeClass("yya"), n.removeClass("yya"), $(".lower").addClass("mymenu"));
 		        o > e && (a.addClass("yya"), n.addClass("yya"), $(".lower").removeClass("mymenu")),
-		        1600 > cWidth && o > t && (a.addClass("gizle"), n.addClass("gizle"), o > i ? (a.removeClass("sabit"), n.removeClass("sabit")) : (a.addClass("sabit"), n.addClass("sabit")), i = o);
+		        1600 > document.body.clientWidth && o > t && (a.addClass("gizle"), n.addClass("gizle"), o > i ? (a.removeClass("sabit"), n.removeClass("sabit")) : (a.addClass("sabit"), n.addClass("sabit")), i = o);
 		});
         },
         XLS: function () {
